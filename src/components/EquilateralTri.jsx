d33b48eff37b1d524e6a3d1da8fae0fce8c26494
Junayed Jihad <junayedjihad48@gmail.com>
2023-11-06 22:24:45 +0600

    First Commit

--- src/components/EquilateralTri.jsx
+++ src/components/EquilateralTri.jsx
@@ -5,17 +5,17 @@ const EquilateralTri = () => {
      const[result,setResult]=useState(0)
      function handleSubmit(e) {
           e.preventDefault()
-          setResult(((Math.sqrt(3)/4)*side*side).toFixed(3))
+          setResult(`${((Math.sqrt(3)/4)*side*side).toFixed(3)} sq.unit`)
      }
      return (
           <div>
-               <form onSubmit={handleSubmit} className='p-3 text-light'>
+               <form onSubmit={handleSubmit} className='p-3  text-light'>
                     <p className='mb-1'>Length of a side of the triangle:</p>
                     <input className='form-control mb-3' type="number" min='0' value={side} onChange={(e)=>{setSide(e.target.value)}} />
                     <p className='mb-1'>Area of the triangle:</p>
-                    <input className='form-control' type="number" name="" id="" value={result} disabled />
+                    <input className='form-control' type="text" name="" id="" value={result} disabled />
                     <div className="button text-center  mt-4">
-                    <button className='btn btn-outline-primary' type="submit">Calculate</button>
+                    <button className='btn btn-success ' type="submit">Calculate</button>
                     </div>
 
                </form>
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,7 +4,7 @@ import imgSrc from '../assets/image/Calculator-bro.png'
 import MenuItem from './MenuItem';
 
 const MainLayout = () => {
-    
+
      return (
           <div className='container '>
                <div className="row">
@@ -12,13 +12,13 @@ const MainLayout = () => {
                          <img src={imgSrc} alt="Illustration" />
                     </div>
                     <div className="col-12 col-lg-6 d-flex flex-column justify-content-center gap-2 gap-lg-4 text-light ">
-                         <h2 className='text-center text-lg-start'>Tired of doing boring calculations?</h2>
-                         <h2 className='text-center text-lg-start'>Now focus on the topics as</h2>
+                         <h2 className='text-center text-lg-start mb-0'>Tired of doing boring calculations?</h2>
+                         <h2 className='text-center text-lg-start mb-0'>Now focus on the topics as</h2>
                          <h2 className='text-center text-lg-start'><span className='h1 fw-bold ' style={{color:'#8e44ad'}}>MathAssist</span> is here for you...</h2>
                     </div>
                </div>
                <div className="row mt-4 ">
-                    <div className=" menu_container col-6 p-3 d-flex flex-column gap-2">
+                    <div className=" menu_container order-lg-1 order-2 mt-5 mt-lg-0  col-12 col-lg-6 p-3 d-flex flex-column gap-2">
                          <MenuItem title='Equilateral Triangle'  path="/"/>
                          <MenuItem title='Isoseles Triangle'  path="isoseles"/>
                          <MenuItem title='03 sides of triangle given'/>
@@ -27,7 +27,7 @@ const MainLayout = () => {
                          <MenuItem title='Solve 02 linear equations'/>
                          <MenuItem title='Solve a quadric equation'/>
                     </div>
-                    <div className="col-6 d-flex align-items-center ">
+                    <div className="col-12 col-lg-6 order-lg-2 order-1  d-flex align-items-center justify-content-center  ">
                          <Outlet/>
                     </div>
                </div>
